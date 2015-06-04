ac23b42ba71a81f936af906c4725f1cdb79dfef5
palys <tomek201992@gmail.com>
2015-06-05 00:01:10 +0200

    Add sender mock

---
+++ Mocks/src/sim/mock/SenderMock.java
@@ -0,0 +1,14 @@
+package sim.mock;
+
+import sim.communication.Message;
+import sim.communication.Sender;
+
+public class SenderMock implements Sender {
+
+	@Override
+	public void send(Message<?> message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
